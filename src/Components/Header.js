00ff519f302091f61a8a80d58d1aa8b2bdd4060a
Deniel Salvacion <denielsalvacion@gmail.com>
2024-09-01 13:27:33 +0800

    refactor: update resume link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,55 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 
 const Header = () => {
-    return   <div class="main" id="Home">
-    <header>
-       <div class="logo">
+  return (
+    <div class="main" id="Home">
+      <header>
+        <div class="logo">
           <h3>Deniel Salvacion</h3>
-       </div>
-       <ul class="nav-list">
-          <li><a href="#Home" class="nav-list-item">Home</a></li>
-          <li><a href="#About" class="nav-list-item">About</a></li>
-          <li><a href="#Projects" class="nav-list-item">Projects</a></li>
-          <li><a href="#Contact" class="nav-list-item">Contact</a></li>
-       </ul>
-       <div class="burger">
-           <div class="div-1"></div>
-           <div class="div-2"></div>
-           <div class="div-3"></div>
-       </div>
-    </header>
-    <div class="main-content">
-       <h1>Deniel SalvacionSSSSSSSSSSSSS</h1>
-       <h2>Front End <span>Developer</span></h2>
-       <a href="https://drive.google.com/file/d/1bMX9t0CT4hV1Q1DLRas6_OGGqw0rmOIi/view?usp=sharing" class="btn">Download my resume</a>
+        </div>
+        <ul class="nav-list">
+          <li>
+            <a href="#Home" class="nav-list-item">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#About" class="nav-list-item">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#Projects" class="nav-list-item">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#Contact" class="nav-list-item">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div class="burger">
+          <div class="div-1"></div>
+          <div class="div-2"></div>
+          <div class="div-3"></div>
+        </div>
+      </header>
+      <div class="main-content">
+        <h1>Deniel SalvacionSSSSSSSSSSSSS</h1>
+        <h2>
+          Front End <span>Developer</span>
+        </h2>
+        <a
+          href="https://drive.google.com/file/d/1UBarzjklq9LMW-QKcuVu-w9jnjePX-DH/view?usp=sharing"
+          class="btn"
+        >
+          Download my resume
+        </a>
+      </div>
     </div>
- </div>
-}
+  );
+};
 
 export default Header;
